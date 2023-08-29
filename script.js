8a2e00700797ba0bb9494046020bdcd3748207c6
ysy7 <ysy2471@gmail.com>
2023-08-30 05:46:38 +0900

    cart 버튼 추가

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 		//데이터 정의 JSON 타입으로 작성(추후 DB 연동시 DB 데이터로 대체함)
 		//신발 설명은 일단 여기서 수정하면 됨
 		const data = [
-			{category:'man',id:'01',image:'m1.png',title:'Adidas',comment:'Adidas Yeezy Foam RNNR MX Cinder <br><br>134,000원'}
-		   ,{category:'man',id:'02',image:'m2.png',title:'Jordan',comment:'Jordan 1 Retro Low OG Black Toe <br><br>168,000원'}
-		   ,{category:'man',id:'03',image:'m3.png',title:'Asics',comment:'Asics Gel-Kayano 14 Cream Black <br><br>910,000원'}
-		   ,{category:'man',id:'04',image:'m4.png',title:'Nike',comment:'Nike Air Force 1 07 WB Flax <br><br>165,000원'}
-		   ,{category:'man',id:'05',image:'m5.png',title:'New Balance',comment:'New Balance 530 White Silver <br><br>125,000원'}
-		   ,{category:'man',id:'06',image:'m6.png',title:'Nike x Supreme',comment:'Nike x Supreme Air Force 1 Low White <br><br>174,000원'}
-		   ,{category:'man',id:'07',image:'m7.png',title:'Jordan 1 x Travis Scott',comment:'Jordan 1 x Travis Scott Retro Low OG SP Sail and Ridgerock <br><br>1,391,000원'}
-		   ,{category:'man',id:'08',image:'m8.png',title:'Oofos',comment:'Oofos OOriginal Black <br><br>51,000원'}
+			{category:'man',id:'01',image:'m1.png',title:'Adidas',comment:'Adidas Yeezy Foam RNNR MX Cinder <br><br>134,000원<button>cart</button>'}
+		   ,{category:'man',id:'02',image:'m2.png',title:'Jordan',comment:'Jordan 1 Retro Low OG Black Toe <br><br>168,000원<button>cart</button>'}
+		   ,{category:'man',id:'03',image:'m3.png',title:'Asics',comment:'Asics Gel-Kayano 14 Cream Black <br><br>910,000원<button>cart</button>'}
+		   ,{category:'man',id:'04',image:'m4.png',title:'Nike',comment:'Nike Air Force 1 07 WB Flax <br><br>165,000원<button>cart</button>'}
+		   ,{category:'man',id:'05',image:'m5.png',title:'New Balance',comment:'New Balance 530 White Silver <br><br>125,000원<button>cart</button>'}
+		   ,{category:'man',id:'06',image:'m6.png',title:'Nike x Supreme',comment:'Nike x Supreme Air Force 1 Low White <br><br>174,000원<button>cart</button>'}
+		   ,{category:'man',id:'07',image:'m7.png',title:'Jordan 1 x Travis Scott',comment:'Jordan 1 x Travis Scott Retro Low OG SP Sail and Ridgerock <br><br>1,391,000원<button>cart</button>'}
+		   ,{category:'man',id:'08',image:'m8.png',title:'Oofos',comment:'Oofos OOriginal Black <br><br>51,000원<button>cart</button>'}
 
-		   ,{category:'woman',id:'09',image:'w1.png',title:'Colecole',comment:'Colecole New Wave White <br><br>48,000원'}
-		   ,{category:'woman',id:'10',image:'w2.png',title:'Miu Miu',comment:'Miu Miu Satin Ballerinas Pumps Toe Black <br><br>870,000원'}
-		   ,{category:'woman',id:'11',image:'w3.png',title:'Adidas',comment:'Adidas Bermuda Glow Pink <br><br>179,000원'}
-		   ,{category:'woman',id:'12',image:'w4.png',title:'Adidas',comment:'Adidas Superstar Core Black White <br><br>129,000원'}
-		   ,{category:'woman',id:'13',image:'w5.png',title:'Onitsuka',comment:'Onitsuka Tiger Mexico 66 White Silver <br><br>170,000원'}
-		   ,{category:'woman',id:'14',image:'w6.png',title:'New Balance',comment:'New Balance 574 Legacy Navy <br><br>128,000원'}
-		   ,{category:'woman',id:'15',image:'w7.png',title:'Adidas',comment:'Adidas Gazelle Dark Green Cream White <br><br>113,000원'}
-		   ,{category:'woman',id:'16',image:'w8.png',title:'Nike',comment:'Nike Air Force 1 07 Low Triple Black <br><br>150,000원'}
+		   ,{category:'woman',id:'09',image:'w1.png',title:'Colecole',comment:'Colecole New Wave White <br><br>48,000원<button>cart</button>'}
+		   ,{category:'woman',id:'10',image:'w2.png',title:'Miu Miu',comment:'Miu Miu Satin Ballerinas Pumps Toe Black <br><br>870,000원<button>cart</button>'}
+		   ,{category:'woman',id:'11',image:'w3.png',title:'Adidas',comment:'Adidas Bermuda Glow Pink <br><br>179,000원<button>cart</button>'}
+		   ,{category:'woman',id:'12',image:'w4.png',title:'Adidas',comment:'Adidas Superstar Core Black White <br><br>129,000원<button>cart</button>'}
+		   ,{category:'woman',id:'13',image:'w5.png',title:'Onitsuka',comment:'Onitsuka Tiger Mexico 66 White Silver <br><br>170,000원<button>cart</button>'}
+		   ,{category:'woman',id:'14',image:'w6.png',title:'New Balance',comment:'New Balance 574 Legacy Navy <br><br>128,000원<button>cart</button>'}
+		   ,{category:'woman',id:'15',image:'w7.png',title:'Adidas',comment:'Adidas Gazelle Dark Green Cream White <br><br>113,000원<button>cart</button>'}
+		   ,{category:'woman',id:'16',image:'w8.png',title:'Nike',comment:'Nike Air Force 1 07 Low Triple Black <br><br>150,000원<button>cart</button>'}
 		   
-		   ,{category:'kid',id:'17',image:'k1.png',title:'Golden Goose',comment:'Golden Goose Superstar White Navy Heel Tab Sneakers <br><br>500,000원'}
-		   ,{category:'kid',id:'18',image:'k2.png',title:'Crocs',comment:'Crocs Classic Crystals Pearls Clog White <br><br>42,000원'}
-		   ,{category:'kid',id:'19',image:'k3.png',title:'Burberry',comment:'Burberry Vintage Check Cotton Leather Sneakers Archive Beige <br><br>349,000원'}
-		   ,{category:'kid',id:'20',image:'k4.png',title:'UGG',comment:'UGG Classic Clear Mini II Natural Black <br><br>110,000원'}
-		   ,{category:'kid',id:'21',image:'k5.png',title:'Adidas',comment:'Adidas Yeezy Slide Bone 2022 <br><br>185,000원'}
-		   ,{category:'kid',id:'22',image:'k6.png',title:'Burberry',comment:'Burberry Icon Stripe Logo Print Cotton Gabardine Sneakers Optic White <br><br>270,000원'}
-		   ,{category:'kid',id:'23',image:'k7.png',title:'Nike',comment:'Nike Dunk Low Retro Black <br><br>114,000원'}
-		   ,{category:'kid',id:'24',image:'k8.png',title:'Nike',comment:'Nike Air Zoom Alphafly Next% Flyknit Rawdacious <br><br>399,000원'}
+		   ,{category:'kid',id:'17',image:'k1.png',title:'Golden Goose',comment:'Golden Goose Superstar White Navy Heel Tab Sneakers <br><br>500,000원<button>cart</button>'}
+		   ,{category:'kid',id:'18',image:'k2.png',title:'Crocs',comment:'Crocs Classic Crystals Pearls Clog White <br><br>42,000원<button>cart</button>'}
+		   ,{category:'kid',id:'19',image:'k3.png',title:'Burberry',comment:'Burberry Vintage Check Cotton Leather Sneakers Archive Beige <br><br>349,000원<button>cart</button>'}
+		   ,{category:'kid',id:'20',image:'k4.png',title:'UGG',comment:'UGG Classic Clear Mini II Natural Black <br><br>110,000원<button>cart</button>'}
+		   ,{category:'kid',id:'21',image:'k5.png',title:'Adidas',comment:'Adidas Yeezy Slide Bone 2022 <br><br>185,000원<button>cart</button>'}
+		   ,{category:'kid',id:'22',image:'k6.png',title:'Burberry',comment:'Burberry Icon Stripe Logo Print Cotton Gabardine Sneakers Optic White <br><br>270,000원<button>cart</button>'}
+		   ,{category:'kid',id:'23',image:'k7.png',title:'Nike',comment:'Nike Dunk Low Retro Black <br><br>114,000원<button>cart</button>'}
+		   ,{category:'kid',id:'24',image:'k8.png',title:'Nike',comment:'Nike Air Zoom Alphafly Next% Flyknit Rawdacious <br><br>399,000원<button>cart</button>'}
 	   ];
 	   
 	   $(document).ready(function(){//jquery 화면로딩(html을 모두 불러온후)이 끝나면 스크립트 실행
